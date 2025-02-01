9ff80d026e6fc2c67ee2bd46b14a281207210717
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2025-02-01 01:19:27 +0000

    docker Dockerise the application

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,21 @@
 const express = require('express');
+const webRouter = require('./routes/web');
 const apiRouter = require('./routes/api');
 const webhookRouter = require('./routes/webhook');
 const chalk = require('chalk');
 
 const app = express();
 
-const log = console.log;
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use('/', webRouter);
 app.use('/api', apiRouter);
 app.use('/webhook', webhookRouter);
 
 app.use((err, req, res, next) => {
-    res.status(400).send("Something broke!\n");
-})
-
-app.listen(8080, () => {
-    log(chalk.bold.white("VeriGlyph: Sentinel listening on port 8080\n"));
+    res.status(400).send("Something broke!");
 });
 
-// module.exports = app;
+app.listen(8080, () => {
+    console.log(chalk.bold.white("VeriGlyph: Sentinel listening on port 8080"));
+});
