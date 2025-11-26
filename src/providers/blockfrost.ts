e5a4385389304e9411985bbe9a84a0505d639b86
Adam Dean <adam@crypto2099.io>
2025-11-25 20:40:55 -0700

    Add a log when fetching the metadata page to see what's going on...

--- src/providers/blockfrost.ts
+++ src/providers/blockfrost.ts
@@ -79,6 +79,7 @@ export class BlockfrostProvider implements ProviderClient {
 
   private async fetchMetadataPage(page: number): Promise<BlockfrostMetadataItem[]> {
     const url = `${this.baseUrl}/metadata/txs/labels/${this.config.metadataKey}?order=asc&page=${page}&count=${this.config.pageSize}`;
+    console.log(`Fetching metadata page ${page} from ${url}`);
     const data = await this.fetchJson<BlockfrostMetadataItem[]>(url);
     await this.sleep(this.delayMs);
     return data;
